ced6032d27d9d4b3c24ed0e73733cf631a7df6e3
Mohini56 <91308740+Mohini56@users.noreply.github.com>
2024-03-31 01:19:58 +0530

    Update F1.java

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,6 +4,7 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super(name,4,4,6,false,"7seater",4);
     }
 
     public void accelerate(int rate){
@@ -18,8 +19,28 @@ public class F1 extends Car {
          * speed more than 250: gear 6
          */
 
-        if(newSpeed == 0) {
+      if(newSpeed == 0) {
+            stop();
+            changeGear(1);
             //Stop the car, set gear as 1
+        }
+        else if (newSpeed<=50) {
+            changeGear(1);
+        }
+        else if(newSpeed<=100){
+            changeGear(2);
+        }
+        else if(newSpeed<=150){
+            changeGear(3);
+        }
+        else if(newSpeed<=200){
+            changeGear(4);
+        }
+        else if(newSpeed<=250){
+            changeGear(5);
+        }
+        else{
+            changeGear(6);
         }
         //for all other cases, change the gear accordingly
 
